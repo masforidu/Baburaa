062f28ba85a129b78dc3b070135509dac0f82908
red1wa <shegarlaofficial@gmail.com>
2024-12-20 21:26:27 +0300

    set

--- shegar/settings.py
+++ shegar/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
 # SECURITY WARNING: don't run with debug turned on in production
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = ['shegarlandhub.up.railway.app','localhost','http://127.0.0.1:8000/']
 
 # Application definition
 INSTALLED_APPS = [
