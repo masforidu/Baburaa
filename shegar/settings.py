043ccecd64a6e76098a349ba9b3ed536c0352181
red1wa <shegarlaofficial@gmail.com>
2025-01-02 22:39:34 +0300

    logo

--- shegar/settings.py
+++ shegar/settings.py
@@ -136,9 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/ "staticfiles"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE =  "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
